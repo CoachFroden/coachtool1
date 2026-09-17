ac7f93b5036458c37d39660fb66c13643780a905
CoachFroden <frode@vibliz.com>
2026-09-18 00:20:43 +0200

    Refresh automatic playing time cache in postmatch-playingtime-sync.js

--- postmatch-playingtime-sync.js
+++ postmatch-playingtime-sync.js
@@ -11,7 +11,7 @@ import {
   MANUAL_OVERRIDE_VERSION,
   calculatePlayingTime,
   samePlayingTime
-} from "./postmatch-playingtime-core.js?v=20260918-2";
+} from "./postmatch-playingtime-core.js?v=20260918-3";
 
 export async function recalculateMatchPlayingTime(matchId, options = {}) {
   if (!matchId) return null;
