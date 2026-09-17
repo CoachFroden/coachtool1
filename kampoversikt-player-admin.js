3489daa453c1cdd7f76c34af3d6d33103ac7e29b
CoachFroden <frode@vibliz.com>
2026-09-18 00:10:23 +0200

    Bust playing time module cache in kampoversikt-player-admin.js

--- kampoversikt-player-admin.js
+++ kampoversikt-player-admin.js
@@ -5,13 +5,13 @@ import {
   updateDoc,
   serverTimestamp
 } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
-import { recalculateMatchPlayingTime } from "./postmatch-playingtime-sync.js";
+import { recalculateMatchPlayingTime } from "./postmatch-playingtime-sync.js?v=20260918-1";
 import {
   PLAYING_TIME_SCHEMA_VERSION,
   MANUAL_OVERRIDE_VERSION,
   calculatePlayingTime,
   applyManualOverrides
-} from "./postmatch-playingtime-core.js";
+} from "./postmatch-playingtime-core.js?v=20260918-1";
 
 let activeMatchId = null;
 
